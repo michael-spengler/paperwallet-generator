11d5d60fc0b6cc0949103f6465bde7a8901cbed6
Michael <michael.spengler@sap.com>
2023-08-13 14:37:37 +0200

    add complete barefoot version

--- svelte-ui/src/App.svelte
+++ svelte-ui/src/App.svelte
@@ -1,50 +1,67 @@
 <script>
-	export let name;
-
 	// import Map from "https://deno.land/x/svelte_map/Maps.svelte"
 
 	import { ethers } from "ethers";
 	import QRCode from "qrcode";
 
+	let walletInfos = [];
 	let amountOfWallets = 1;
-	let counter = 0
+	let counter = 0;
+	let ready = false;
 	let resultOfCreateRandom;
 	let walletFromPrivateKey;
-	let canvasAddress = "canvasAddress"
-	let canvasPrivateKey = "canvasPrivateKey"
 
 	async function generateOne() {
-
+		let walletInfo = {};
 		resultOfCreateRandom = ethers.Wallet.createRandom();
 		walletFromPrivateKey = new ethers.Wallet(
 			resultOfCreateRandom.chainCode
 		);
 
-		QRCode.toCanvas(
-			document.getElementById(canvasAddress+counter),
-			walletFromPrivateKey.address,
-			function (error) {
-				if (error) console.error(error);
-				console.log("qr code addresssuccess!");
-			}
-		);
-		QRCode.toCanvas(
-			document.getElementById(canvasPrivateKey+counter),
-			walletFromPrivateKey.privateKey,
-			function (error) {
-				if (error) console.error(error);
-				console.log("qr code private key success!");
-			}
-		);
+		walletInfo.publicKey = walletFromPrivateKey.address;
+		walletInfo.privateKey = walletFromPrivateKey.privateKey;
+		walletInfo.mnemonic = resultOfCreateRandom.mnemonic.phrase;
+		walletInfo.canvasIDPublicKey = `canvasAddress${counter}`;
+		walletInfo.canvasIDPrivateKey = "canvasPrivateKey" + counter;
+
+		return walletInfo;
+	}
+
+	function prepareQRCodes(data) {
+		for (const walletInfo of data) {
+			QRCode.toCanvas(
+				document.getElementById(walletInfo.canvasIDPublicKey),
+				walletInfo.publicKey,
+				{ errorCorrectionLevel: "M", version: 4 },
+				function (error) {
+					if (error) console.error(error);
+					console.log("qr code addresssuccess!");
+				}
+			);
+			QRCode.toCanvas(
+				document.getElementById(walletInfo.canvasIDPrivateKey),
+				walletInfo.privateKey,
+				{ errorCorrectionLevel: "M", version: 5 },
+				function (error) {
+					if (error) console.error(error);
+					console.log("qr code private key success!");
+				}
+			);
+		}
 	}
 	async function generateAll() {
+		walletInfos = [];
 
-		while (counter < amountOfWallets){
-			await generateOne()
-			counter++
+		while (counter < amountOfWallets) {
+			counter++;
+			let walletInfo = await generateOne();
+			walletInfos.push(walletInfo);
 		}
-		
-		counter = 0
+		ready = true;
+
+		setTimeout(() => {
+			prepareQRCodes(walletInfos);
+		}, 1 * 200);
 	}
 </script>
 
@@ -60,33 +77,42 @@
 		</div>
 	</div>
 
-	{#if walletFromPrivateKey}
-		<h2>Public Key (Share)</h2>
-		{walletFromPrivateKey.address} <br />
-	{/if}
-	<canvas id={canvasAddress+counter}/>
-	<br />
-	{#if walletFromPrivateKey}
-		<h2>Private (Do not share)</h2>
-		<b> Key: </b>
-		{walletFromPrivateKey.privateKey} <br />
-		<br />
-	{/if}
-	<canvas id={canvasPrivateKey+counter} />
-	<p />
-	{#if resultOfCreateRandom}
-		<b> Mnemonic: </b>
-		{resultOfCreateRandom.mnemonic.phrase}
-
-		<p><br /></p>
-		<button class="b1" onclick="window.print()">Print</button>
-	{/if}
+	<div class="center">
+		{#if ready}
+			{#each walletInfos as wi}
+			<a href="https://cultmagazine.org" target="_blank">
+				<h4>cultmagazine.org</h4>
+			</a>
+			<div class="small">
+			<b>
+				Public Key (Share):
+			</b>
+				{wi.publicKey} <br />
+				<canvas id={wi.canvasIDPublicKey} />
+				<p></p>
+				<b> Private Key (do not share): </b>
+					{wi.privateKey} <br />
+				<b> Mnemonic (do not share): </b>
+					{wi.mnemonic}
+					<br />
+					
+					
+					<canvas id={wi.canvasIDPrivateKey} />
+				</div>
+			{/each}
+		{/if}
+		{#if walletInfos.length === amountOfWallets}
+		<div class="noprint">
+			<button class="b1" onclick="window.print()">Print</button>
+		</div>
+		{/if}
+	</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
+		padding: 0em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
@@ -98,6 +124,11 @@
 		font-weight: 100;
 	}
 
+	.small{
+		margin-left: 0;
+		padding-left: 0;
+		font-size: 9px;
+	}
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
